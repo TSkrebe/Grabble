4553c40c1ff4a839b2c75b7e86299ef14b48cb02
Titas Skrebe <titas.skrebe@gmail.com>
2017-01-22 16:40:07 +0000

    Fixed tests

--- app/src/androidTest/java/com/skrebe/titas/grabble/FlowTest.java
+++ app/src/androidTest/java/com/skrebe/titas/grabble/FlowTest.java
@@ -31,6 +31,16 @@ import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.is;
 
+/**
+ * Goes to "Add a word" activity
+ * Writes a word and click "add" button
+ * Goes back to map
+ * Goes to statistics activity
+ * Goes to settings activity
+ * Changes difficulty to Easy
+ * Goes back to map
+ * Clicks on FAB
+ */
 @LargeTest
 @RunWith(AndroidJUnit4.class)
 public class FlowTest {
@@ -47,72 +57,70 @@ public class FlowTest {
                         isDisplayed()));
         button.perform(click());
 
-        ViewInteraction appCompatAutoCompleteTextView = onView(
-                allOf(withId(R.id.autoCompleteTextView), isDisplayed()));
-        appCompatAutoCompleteTextView.perform(replaceText("thallus"), closeSoftKeyboard());
+        onView(
+                allOf(withId(R.id.autoCompleteTextView), isDisplayed()))
+        .perform(replaceText("thallus"), closeSoftKeyboard());
 
-        ViewInteraction appCompatTextView = onView(
-                allOf(withText("thallus"), isDisplayed()));
-        appCompatTextView.perform(click());
+        onView(
+                allOf(withText("thallus"), isDisplayed()))
+        .perform(click());
 
-        ViewInteraction appCompatButton = onView(
-                allOf(withId(R.id.addWord), withText("Add"), isDisplayed()));
-        appCompatButton.perform(click());
+        onView(
+                allOf(withId(R.id.addWord), withText("Add"), isDisplayed())).perform(click());
 
-        ViewInteraction appCompatImageButton = onView(
+        onView(
                 allOf(withContentDescription("Navigate up"),
                         withParent(withId(R.id.toolbar)),
-                        isDisplayed()));
-        appCompatImageButton.perform(click());
+                        isDisplayed()))
+                .perform(click());
 
-        ViewInteraction button2 = onView(
+        onView(
                 allOf(withText("Statistics"),
                         withParent(allOf(withId(R.id.map),
                                 withParent(withId(android.R.id.content)))),
-                        isDisplayed()));
-        button2.perform(click());
+                        isDisplayed()))
+                .perform(click());
 
         openActionBarOverflowOrOptionsMenu(getInstrumentation().getTargetContext());
 
-        ViewInteraction appCompatTextView2 = onView(
-                allOf(withId(R.id.title), withText("Settings"), isDisplayed()));
-        appCompatTextView2.perform(click());
+        onView(
+                allOf(withId(R.id.title), withText("Settings"), isDisplayed()))
+                .perform(click());
 
-        ViewInteraction linearLayout = onView(
+        onView(
                 allOf(childAtPosition(
                         withId(android.R.id.list),
                         0),
-                        isDisplayed()));
-        linearLayout.perform(click());
+                        isDisplayed()))
+                .perform(click());
 
-        ViewInteraction appCompatCheckedTextView = onView(
+        onView(
                 allOf(withId(android.R.id.text1), withText("Easy"),
                         childAtPosition(
                                 allOf(withClassName(is("com.android.internal.app.AlertController$RecycleListView")),
                                         withParent(withClassName(is("android.widget.FrameLayout")))),
                                 0),
-                        isDisplayed()));
-        appCompatCheckedTextView.perform(click());
+                        isDisplayed())).perform(click());
 
-        ViewInteraction appCompatImageButton2 = onView(
+        onView(
                 allOf(withContentDescription("Navigate up"),
                         withParent(allOf(withId(R.id.settingsToolbar),
                                 withParent(withId(R.id.settingsToolbarParent)))),
-                        isDisplayed()));
-        appCompatImageButton2.perform(click());
+                        isDisplayed()))
+                .perform(click());
 
-        ViewInteraction appCompatImageButton3 = onView(
+        onView(
                 allOf(withContentDescription("Navigate up"),
                         withParent(withId(R.id.statsToolbar)),
-                        isDisplayed()));
-        appCompatImageButton3.perform(click());
+                        isDisplayed()))
+                .perform(click());
 
-        ViewInteraction floatingActionButton = onView(
+        onView(
                 allOf(withId(R.id.fab),
                         withParent(allOf(withId(R.id.clayout),
                                 withParent(withId(R.id.map)))),
-                        isDisplayed()));
-        floatingActionButton.perform(click());
+                        isDisplayed()))
+                .perform(click());
 
 
     }
